e507493638f4d707075c435d8ca9a69a9dad850b
Amal Ben Kilani <eng.ben.kilani.amal@gmail.com>
2023-01-10 21:34:29 +0100

    UPDATE

--- server/routes/doctorRoutes.js
+++ server/routes/doctorRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const multer = require('multer')
 
 const storage = multer.diskStorage({
-    destination: 'uploads/',
+    destination: 'tmp/uploads/',
 
     filename: function (req, file, cb) {
             cb(null,Date.now() + '-' + file.originalname )
